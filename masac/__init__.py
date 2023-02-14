9da4981a9cfe24e00221e0814ad905aa543eabdc
Florian Felten <florian.felten@uni.lu>
2023-02-14 09:31:55 +0100

    Initial commit

---
+++ masac/__init__.py
@@ -0,0 +1,1 @@
+"""This package contains the implementation of the Multi-Agent SAC (MASAC) algorithm."""
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+setup(
+    name="masac",
+    description="Multi-Agent SAC implementation for PettingZoo",
+    version="0.1",
+)
